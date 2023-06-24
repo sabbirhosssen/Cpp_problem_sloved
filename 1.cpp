690257971423be30d89ef3516b058e6e390462cf
sabbirhosssen <sabbirhossen58sh58@gmail.com>
2023-06-24 11:12:37 +0600

    first commit

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	printf(" bfb \n");
+
+
+
+
+	return 0;
+}
---
+++ delete.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+
+   char a[100];
+   gets(a[]);
+   printf("%s",a );
+
+   return 0;
+}
