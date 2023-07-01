26996755e57ba8b38f67eb123689af60ccbf4d76
sabbirhosssen <sabbirhossen58sh58@gmail.com>
2023-07-01 12:55:33 +0600

    problem life universe and everything

---
+++ life_universe_and_everything42.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+	while(true)
+	{
+		int x;
+		cin>>x;
+		if(x==42){
+			break;
+		}
+		cout<<x<<endl;
+	}
+
+	return 0;
+	///input= 1 22 13 16 27 42 58 97
+	// Output= 1 22 13 16 27
+}
