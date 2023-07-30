97a3b6adb88f17b831ec10507e13c6fc6cb7832c
sabbirhosssen <sabbirhossen58sh58@gmail.com>
2023-07-30 20:47:35 +0600

    test Cases

--- Testcases.cpp
+++ Testcases.cpp
@@ -4,6 +4,11 @@ int main()
 {
    int t;
    cin>>t;
+   //Write a test cases in two way;
+   //first for loop use (but test variable declear extra time and extra memory space)
+       //  for (int test= 0; test <= t; ++test) //
+   //second is while loop (Condition is {t--}   t?=0 is it's running )
+   //Then while loop is best 
    while (t--)
    {
     int n;
